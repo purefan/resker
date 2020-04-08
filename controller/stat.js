f442de081a7ade9b5f171ccc8958737ffe381f6e
Miguel Espinoza <wiseit@outlook.com>
2020-04-08 22:13:29 +0200

    Embryo for fetching stats

--- controller/stat.js
+++ controller/stat.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const auth = require('../middleware/auth')
 const model = require('../model')
 
-router.get('/stats', (req, res) => {
-    const stats = model.stats.fetch()
-    console.log('stats', stats)
+router.get('/stats', async (req, res) => {
+    const stats = await model.stats.fetch()
+    res.setHeader('Cache-Control', 'public, max-age=3600') // 1 hr
     res.send(stats)
 })
 
